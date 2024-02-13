f14f71773695e50e7ee53d2f91b5c178a541fa44
archananarayanan <archananarayanan223@gmail.com>
2024-02-12 22:40:22 -0500

    backdrop-blur

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -273,6 +273,7 @@ const handleSearchChange = (e: { target: { value: string; }; }) => {
 
   return (
     <div className="flex w-full min-h-full px-1 align-center lg:px-2">
+      <div className="fixed inset-0 bg-gray-500/30 bg-opacity-75 transition-opacity"></div>
       <div className="flex pt-4 px-10 ml-3 w-full min-h-[700px]">
         <div>
           {/* Ststes & header */}
@@ -496,7 +497,7 @@ const handleSearchChange = (e: { target: { value: string; }; }) => {
           </div>
         </div>
       </div>
-      { stockModal && <div id="stock-modal" className="flex overflow-y-auto overflow-x-hidden fixed align-center z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      { stockModal && <div id="stock-modal" className="flex backdrop-blur transform overflow-y-auto overflow-x-hidden fixed align-center z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div className="relative p-4 w-full max-w-2xl max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
@@ -587,7 +588,7 @@ const handleSearchChange = (e: { target: { value: string; }; }) => {
           </div>
         </div>
       </div>}
-      { categoryModal && <div id="category-modal" className="flex overflow-y-auto overflow-x-hidden fixed align-center z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      { categoryModal && <div id="category-modal" className="flex backdrop-blur transform overflow-y-auto overflow-x-hidden fixed align-center z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div className="relative p-4 w-full max-w-2xl max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
@@ -618,7 +619,7 @@ const handleSearchChange = (e: { target: { value: string; }; }) => {
           </div>
         </div>
       </div>}
-      { tagModal && <div id="tag-modal" className="flex overflow-y-auto overflow-x-hidden fixed align-center z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      { tagModal && <div id="tag-modal" className="flex backdrop-blur transform overflow-y-auto overflow-x-hidden fixed align-center z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div className="relative p-4 w-full max-w-2xl max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
